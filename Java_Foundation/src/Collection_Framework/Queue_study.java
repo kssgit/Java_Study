a407fdd81b11d623eea70af4baa6fc65217791ab
kssgit <76714467+kssgit@users.noreply.github.com>
2021-08-04 19:33:07 +0900

    Study Stack and Queue

---
+++ Java_Foundation/src/Collection_Framework/Queue_study.java
@@ -0,0 +1,24 @@
+package Collection_Framework;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queue_study {
+	public static void main(String[] args) {
+		Queue<Integer> q = new LinkedList<>();
+		
+		// 삽입 - offer
+		// 추출 - poll
+		q.offer(5);
+		q.offer(2);
+		q.offer(4);
+		q.offer(1);
+		q.offer(6);
+		q.poll();
+		q.offer(9);
+		//먼저 들어온 원소부터 추출
+		while(!q.isEmpty()) {
+			System.out.println(q.poll()+" ");
+		}
+	}
+}
---
+++ Java_Foundation/src/Collection_Framework/Stack_study.java
@@ -0,0 +1,25 @@
+package Collection_Framework;
+
+import java.util.*;
+
+public class Stack_study {
+	public static void main(String[] args) {
+		Stack<Integer> s = new Stack<>();
+		//삽입 - push
+		//추출 - pop
+		//최상단 원소 출력 - peek
+		s.push(5);
+		s.push(4);
+		s.push(3);
+		s.pop();
+		s.push(1);
+		s.pop();
+		// 스택의 최상단 원소부터 출력
+		while(!s.empty()) {
+			System.out.println(s.peek()+" ");
+			s.pop();
+			
+		}
+		
+	}
+}
