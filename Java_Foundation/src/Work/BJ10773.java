ffd5f05b903f27d5c9582f46be06b1590a3d3e01
kssgit <76714467+kssgit@users.noreply.github.com>
2021-08-22 11:32:06 +0900

    Study Java 0822

--- Java_Foundation/src/Work/BJ10773.java
+++ Java_Foundation/src/Work/BJ10773.java
@@ -5,14 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class BJ10773_제로{
+public class BJ10773{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		test 수 입력
+
 		int T = Integer.parseInt(br.readLine());
-//		Stack 선언
+
 		Stack<Integer> s = new Stack<Integer>();
-//		stack에 값 넣기
+
 		for(int i = 0 ; i < T;i++) {
 			int a = Integer.parseInt(br.readLine());
 			if(a == 0) {
@@ -21,7 +21,7 @@ public class BJ10773_제로{
 				s.push(a);
 			}
 		}
-//		stack 값 더하기
+
 		int sum = 0;
 		int size = s.size();
 		for(int i = 0 ; i < size;i++) {
