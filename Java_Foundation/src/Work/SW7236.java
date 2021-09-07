454a375354036556e19370b9a67988f0c543de7f
kssgit <76714467+kssgit@users.noreply.github.com>
2021-09-07 09:19:51 +0900

    Study Java

--- Java_Foundation/src/Work/SW7236.java
+++ Java_Foundation/src/Work/SW7236.java
@@ -12,20 +12,27 @@ public class SW7236 {
 			int S = Integer.parseInt(br.readLine());
 			char[][] M = new char[S][S];
 			for(int j = 0 ; j < S ; j++) {
-				String[] s = br.readLine().split(" ");
+//				String[] s = br.readLine().split(" ");
+				StringTokenizer token = new StringTokenizer(br.readLine());
 				for(int k = 0 ; k < S ;k++ ) {
-					M[j][k] = s[k].charAt(0);
+					M[j][k] = token.nextToken().charAt(0);
 				}
 			}//2차원 배열 받기
+			
+//			System.out.println(Arrays.deepToString(map));	//2차원 배열 확인하는법
+//			System.out.println(Arrays.toString(a)); 일차원 배열 확인하는법
+			
 			// 상 하 좌 우 좌상 우상 좌하 우하
 			int[] dx = {0,0,-1,1,-1,1,-1,1};
 			int[] dy = {-1, 1, 0,0,-1,-1,1,1};
 			int max = 0;
+
 			for(int j = 0 ; j < S ; j++) {
 				for(int k = 0 ; k < S; k++) {
 					if(M[j][k]=='W') {
 						//8방 탐색
 						int count =0;
+
 						for(int l = 0 ; l <8 ;l++) {
 							//범위 지정
 							if(j+dx[l]>=0 && j+dx[l]<S && k+dy[l]>=0 && k+dy[l]<S) {
