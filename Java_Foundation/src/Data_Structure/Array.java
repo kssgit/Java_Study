0044bd86b168ed70dae9f0c017c9f151619694de
kssgit <76714467+kssgit@users.noreply.github.com>
2021-09-29 22:27:04 +0900

    Update Array.java

--- Java_Foundation/src/Data_Structure/Array.java
+++ Java_Foundation/src/Data_Structure/Array.java
@@ -1,7 +1,13 @@
 package Data_Structure;
 
+import java.util.ArrayList;
+
 public class Array {
 	public static void main(String[] args) {
-		
+		ArrayList<Integer> array = new ArrayList<>();
+		array.add(1);
+		array.add(3);
+		array.remove(1);
+		System.out.println(array);
 	}
 }
