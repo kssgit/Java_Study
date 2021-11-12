6fa037e14e33ab367a1fba9d87d9ee95df14b131
kssgit <76714467+kssgit@users.noreply.github.com>
2021-11-12 23:49:27 +0900

    Update PG프린터.java

--- Java_Foundation/src/프로그래머스/PG프린터.java
+++ Java_Foundation/src/프로그래머스/PG프린터.java
@@ -1,34 +1,35 @@
 package 프로그래머스;
 
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class PG프린터 {
 	public static void main(String[] args) {
-		int[] priorities = {2,1,3,2};
-		int location = 2 ;
+		int[] priorities = {1,1,9,1,1,1};
+		int location = 0 ;
 		PG프린터 pg = new PG프린터();
 		System.out.println(pg.solution(priorities,location));
 	}
 	public int solution(int[] priorities, int location) {
         int answer = 0;
         //우선순위 큐 or 힙 
-        PriorityQueue<Node> pq = new PriorityQueue<Node>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int i = 0 ; i < priorities.length ; i++) {
-        	pq.add(new Node(priorities[i],i));
+        	pq.add(priorities[i]);
         	
         }
-        for(int i = 0 ; i < priorities.length ; i++) {
-        	System.out.println(pq.poll().i);
-        }
-        return answer;
-    }
-}
-class Node{
-	int j ; // 노드의 중요도
-	int i ;// 노드의 순서
-	public Node(int j , int i) {
-		// TODO Auto-generated constructor stub
-		this.i = i;
-		this.j = j;
+        while(!pq.isEmpty()) {
+	        for(int i = 0 ; i < priorities.length ; i++) {
+	        	if(priorities[i] == pq.peek()) {
+	        		if(i == location) {
+	        			answer++;
+	        			return answer;
+	        		}
+	        		pq.poll();
+	        		answer++;
+	        	}
+	        }//for
+        }//while
+        return -1;
 	}
 }
